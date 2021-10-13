3ddc07e8f5c4ab5118632f86f524ee55107e4dab
rgoodman <rawhideron@gmail.com>
2021-10-13 13:45:55 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+
+	provider "aws" {
+		region = "us-east-1"
+	}
+
+	resource "aws_instance" "example" {
+		ami = "ami-02e136e904f3da870"
+		instance_type = "t2.micro"
+		tags = {
+			Name = "terraform-example"
+		}
+	}
+
+
