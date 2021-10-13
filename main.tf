8a4cdef75102aa188bf3395d572d3072726bf2b2
rgoodman <rawhideron@gmail.com>
2021-10-13 19:58:00 -0400

    finally solved the security id problem

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@
 	resource "aws_instance" "example" {
 		ami = "ami-02e136e904f3da870"
 		instance_type = "t2.micro"
+		vpc_security_group_ids = [aws_security_group.instance.id]
 		tags = {
 			Name = "terraform-example"
 		user_data = <<-EOF
