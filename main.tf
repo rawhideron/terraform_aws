f6eb2b32624a2c0ec26669a36e4db406f8e3bbcc
rgoodman <rawhideron@gmail.com>
2021-10-13 20:27:36 -0400

    nothing

--- main.tf
+++ main.tf
@@ -10,23 +10,28 @@
 		vpc_security_group_ids = [aws_security_group.instance.id]
 		tags = {
 			Name = "terraform-example"
+		}
 		user_data = <<-EOF
 			#!/bin/bash
 			echo "Hello, World" > index.html
 			nohup busybox httpd -f -p 8080 &
 			EOF
-		}
+		
 	}
 
 	resource "aws_security_group" "instance" {
 		name = "terraform-example-instance"
+		tags = {
+			Name = "terraform-example"
+		}
 		ingress {
 			from_port = 8080
 			to_port = 8080
 			protocol = "tcp"
 			cidr_blocks = ["0.0.0.0/0"]
-
 		}
+		
+		
 	}
 
 
