2c7249f1c7901de4396dc80afeadffd6b3f645f6
rgoodman <rawhideron@gmail.com>
2021-10-17 12:40:07 -0400

    checking for typos

--- main.tf
+++ main.tf
@@ -112,10 +112,10 @@
 		}
 
 		egress {
-			from_port = 0
-			to_port = 0
-			protocol = "-1"
-			cidr_blocks = ["0.0.0.0/0"]
+			from_port   = 0
+  			to_port     = 0
+  			protocol    = "-1"
+  			cidr_blocks = ["0.0.0.0/0"]
 			
 		}
 	}
