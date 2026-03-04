fdfd4edfead2b894431e6907aab2d6eb75377e3e
Ron Goodman <rawhideron@gmail.com>
2026-03-04 10:58:08 -0500

    changed the method of finding the correct ami to user

--- main.tf
+++ main.tf
@@ -1,115 +1,24 @@
-
-
 provider "aws" {
   region = "us-east-1"
 }
-variable "server_port" {
-  description = "No comment"
-  type        = number
-  default     = 80
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-
-data "aws_vpc" "default" {
-  default = true
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 }
 
+resource "aws_security_group" "lb" {
+  vpc_id = aws_vpc.main.id
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-
-}
-
-
-
-resource "aws_launch_configuration" "example2" {
-  image_id        = "ami-02e136e904f3da870"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-			#!/usr/bin/bash
-			sudo yum install -y httpd &&
-			sudo systemctl start httpd &&
-			sudo systemctl enable httpd &&
-			echo "Hello, World" > index.html
-			EOF
-  lifecycle {
-    create_before_destroy = true
-  }
-
-}
-
-resource "aws_autoscaling_group" "example2" {
-  launch_configuration = aws_launch_configuration.example2.name
-  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
-  target_group_arns    = [aws_lb_target_group.asg.arn]
-  health_check_type    = "ELB"
-  min_size             = 2
-  max_size             = 10
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example2"
-    propagate_at_launch = true
-
-  }
-
-}
-resource "aws_lb" "example2" {
-  name               = "terraform-asg-example2"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default.ids
-  security_groups    = [aws_security_group.alb.id]
-
-}
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example2.arn
-  port              = 80
-  protocol          = "HTTP"
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-  condition {
-    path_pattern {
-
-      values = ["*"]
-    }
-  }
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-
-  }
-}
-resource "aws_lb_target_group" "asg" {
-  name     = "terraform-asg-example"
-  port     = var.server_port
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-
-  }
-}
-resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
   ingress {
     from_port   = 80
     to_port     = 80
@@ -117,28 +26,24 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance2"
-  tags = {
-    Name = "terraform-example2"
-  }
+resource "aws_security_group" "ec2" {
+  vpc_id = aws_vpc.main.id
 
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   ingress {
     from_port   = 22
     to_port     = 22
@@ -146,17 +51,107 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb.id]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-
 }
 
-output "alb_dns_name" {
-  value       = aws_lb.example2.dns_name
-  description = "The domain of the load balancer"
+# ✅ Latest official Ubuntu 24.04 (Noble) AMI in us-east-1 (Canonical)
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  # Optional safety filter (prevents weird edge cases)
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+resource "aws_launch_template" "nginx" {
+  name = "nginx-launch-template"
+
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  user_data = base64encode(<<EOT
+#!/bin/bash
+apt-get update -y
+apt-get install -y nginx certbot python3-certbot-nginx
+systemctl start nginx
+EOT
+  )
+
+  network_interfaces {
+    security_groups             = [aws_security_group.ec2.id]
+    associate_public_ip_address = true
+  }
+}
+
+resource "aws_lb" "app" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb.id]
+  subnets            = aws_subnet.public[*].id
+}
+
+resource "aws_lb_target_group" "web" {
+  name     = "web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+}
+
+resource "aws_autoscaling_group" "web" {
+  desired_capacity = 3
+  min_size         = 3
+  max_size         = 5
+
+  vpc_zone_identifier = aws_subnet.public[*].id
+
+  launch_template {
+    id      = aws_launch_template.nginx.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.web.arn]
+
+  tag {
+    key                 = "Name"
+    value               = "web-server"
+    propagate_at_launch = true
+  }
 }
